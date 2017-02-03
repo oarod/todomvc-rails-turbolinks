ed9ea54efaccc6f2b5a3df2fba7523b05915b1e7
oarod <oscar.arod85@gmail.com>
2017-02-03 16:46:15 -0500

    Implement index and create

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,4 +2,17 @@ class TodosController < ApplicationController
   def index
     @todos = Todo.all
   end
+
+  def create
+    @todo = Todo.new(todo_params)
+
+    if @todo.save
+      redirect_to todos_url
+    end
+  end
+
+  private
+    def todo_params
+      params.require(:todo).permit(:text)
+    end
 end
