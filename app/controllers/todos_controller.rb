def398beb61ff3115b783d97e4dfaf0f21b3c4e8
oarod <oscar.arod85@gmail.com>
2017-02-04 23:53:19 -0500

    Improve routing

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,48 +2,61 @@ class TodosController < ApplicationController
   before_action :set_todo, only: [:update, :destroy]
 
   def index
-    @todos =
-      case filter_params
-      when 'active'
-        Todo.where(completed: false)
-      when 'completed'
-        Todo.where(completed: true)
-      else
-        Todo.all
-      end
+    set_todos
+    save_filter
   end
 
   def create
     if Todo.create(todo_params)
-      redirect_to todos_url
+      redirect_with_filter
     end
   end
 
   def update
     @todo.update(todo_params)
-    redirect_to todos_url
+    redirect_with_filter
   end
 
   def update_many
     Todo.update_all(todo_params.to_h)
-    redirect_to todos_url
+    redirect_with_filter
   end
 
   def destroy
     if @todo.destroy
-      redirect_to todos_url
+      redirect_with_filter
     end
   end
 
   def destroy_many
     if Todo.where(id: params[:ids]).destroy_all
-      redirect_to todos_url
+      redirect_with_filter
     end
   end
 
   private
     def set_todo
       @todo = Todo.find(params[:id])
+    end
+
+    def set_todos
+      @todos =
+        case filter_params
+        when 'active'
+          Todo.where(completed: false)
+        when 'completed'
+          Todo.where(completed: true)
+        else
+          Todo.all
+        end
+    end
+
+    def save_filter
+      session[:filter] = filter_params
+    end
+
+    def redirect_with_filter
+      redirect_to todos_url(filter: session[:filter])
     end
 
     def filter_params
